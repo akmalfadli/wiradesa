53ac05eaa558f43844bf7b7dafd5d3188284e293
Akmal Fadli <akmalfadli94@gmail.com>
2025-08-16 13:02:31 +0700

    theme

--- desa/themes/perwira/resources/views/template.blade.php
+++ desa/themes/perwira/resources/views/template.blade.php
@@ -7,9 +7,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Website Resmi Desa Dayeuhkolot')</title>
-    @include('theme::commons.meta')
-    @include('theme::commons.source_css')
-    @include('theme::commons.source_js')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+        @include('theme::commons.meta')
+        @include('theme::commons.source_css')
+        @include('theme::commons.source_js')
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
 </head>
@@ -18,7 +20,21 @@
         @include('theme::partials.header')
         @include('theme::partials.hero')
         
-        @yield('layout')
+        <div class="px-4 md:px-6 lg:px-8">
+            <div class="flex flex-col md:flex-row gap-8 mt-8">
+                @include('theme::partials.history')
+                @include('theme::partials.location')
+            </div>
+            
+            <div class="flex flex-col md:flex-row gap-8 mt-16">
+                @include('theme::partials.development')
+                @include('theme::partials.vision')
+            </div>
+            
+            @include('theme::partials.statistics')
+            @include('theme::partials.articles')
+            @include('theme::partials.officials')
+        </div>
     </div>
     
     @include('theme::partials.footer')
--- storage/framework/views/403ea3d206be4ef64e57cdfe39380643.php
+++ storage/framework/views/403ea3d206be4ef64e57cdfe39380643.php
@@ -7,9 +7,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $__env->yieldContent('title', 'Website Resmi Desa Dayeuhkolot'); ?></title>
-    <?php echo $__env->make('theme::commons.meta', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('theme::commons.source_css', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('theme::commons.source_js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+        <?php echo $__env->make('theme::commons.meta', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <?php echo $__env->make('theme::commons.source_css', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <?php echo $__env->make('theme::commons.source_js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
 </head>
@@ -18,7 +20,21 @@
         <?php echo $__env->make('theme::partials.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         <?php echo $__env->make('theme::partials.hero', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         
-        <?php echo $__env->yieldContent('layout'); ?>
+        <div class="px-4 md:px-6 lg:px-8">
+            <div class="flex flex-col md:flex-row gap-8 mt-8">
+                <?php echo $__env->make('theme::partials.history', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                <?php echo $__env->make('theme::partials.location', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            </div>
+            
+            <div class="flex flex-col md:flex-row gap-8 mt-16">
+                <?php echo $__env->make('theme::partials.development', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                <?php echo $__env->make('theme::partials.vision', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            </div>
+            
+            <?php echo $__env->make('theme::partials.statistics', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php echo $__env->make('theme::partials.articles', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php echo $__env->make('theme::partials.officials', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        </div>
     </div>
     
     <?php echo $__env->make('theme::partials.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
